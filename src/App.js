c37eb219260c117bdd921714c637c79b2ce091e3
Aryanabinesh <your_new_email@example.com>
2025-03-05 22:18:53 +0530

    main file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import Filters from './components/Filters';
+import Items from './components/Items';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Filters/>
+      <Items/>
     </div>
   );
 }
